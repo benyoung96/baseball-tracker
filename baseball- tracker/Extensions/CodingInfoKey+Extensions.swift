0d7e8d7dc0b475068f211ac6f22c75e31548519e
benyoung96 <benyoung@iastate.edu>
2019-02-24 18:43:16 -0600

    Started injected the persistent container into the needed controllers and conformed the Team model to codable

---
+++ baseball- tracker/Extensions/CodingInfoKey+Extensions.swift
@@ -0,0 +1,11 @@
+//
+//  CodingTeamInfoKey+Extensions.swift
+//  baseball- tracker
+//
+//  Created by Ben Young on 2/24/19.
+//  Copyright © 2019 Ben Young. All rights reserved.
+//
+
+import Foundation
+
+public extension CodingUserInfoKey
